0698a99f41c66d3f4b55c78512342b8d0c3d2cb9
rt022748 <ramsey.tisher@cerner.com>
2020-09-04 23:31:38 -0400

    initial checkin

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
+    title: `Ramsey & Cindy's Easy To Use Data Thing`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
     author: `@gatsbyjs`,
   },
@@ -13,7 +13,16 @@ module.exports = {
         path: `${__dirname}/src/images`,
       },
     },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `data`,
+        path: `${__dirname}/src/data/`,
+      },
+    },
+    `gatsby-transformer-csv`,
     `gatsby-transformer-sharp`,
+    `gatsby-plugin-antd`,
     `gatsby-plugin-sharp`,
     {
       resolve: `gatsby-plugin-manifest`,
@@ -21,12 +30,13 @@ module.exports = {
         name: `gatsby-starter-default`,
         short_name: `starter`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#000000`,
+        theme_color: `#000000`,
         display: `minimal-ui`,
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
+
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,22 +1,65 @@
-import React from "react"
-import { Link } from "gatsby"
+import React, { useState, useEffect } from "react"
+import { useStaticQuery, graphql } from "gatsby"
 
+import { Select } from "antd"
 import Layout from "../components/layout"
-import Image from "../components/image"
-import SEO from "../components/seo"
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link> <br />
-    <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
-  </Layout>
-)
+const { Option } = Select
+
+const IndexPage = () => {
+  const data = useStaticQuery(graphql`
+    query GetCountyData {
+      allCountyTestRatesCsv {
+        nodes {
+          County
+          FEMA_region
+          FIPS_code
+          Percent_Positive_in_prior_7_days
+          Population
+          State
+          Test_Positivity_Classification
+        }
+      }
+    }
+  `)
+
+  const [value, setValue] = useState(null)
+  const [options, setOptions] = useState([])
+  const [search, setSearch] = useState("")
+  const [detail, setDetail] = useState(null)
+
+  function handleChange(value) {
+    console.log(`selected ${value}`)
+  }
+
+  useEffect(() => {
+    const selected = data.allCountyTestRatesCsv.nodes.filter(({ County }) => {
+      return County === value
+    })
+    console.log("Selected", selected)
+    setDetail(selected[0])
+  }, [value])
+
+  return (
+    <Layout>
+      <Select
+        showSearch
+        size="large"
+        style={{ width: 500 }}
+        placeholder="Select a county"
+        onChange={value => setValue(value)}
+      >
+        {data.allCountyTestRatesCsv.nodes.map(({ County }) => (
+          <Option value={County}>{County}</Option>
+        ))}
+      </Select>
+      {detail ? (
+        <pre>{JSON.stringify(detail, null, 2)}</pre>
+      ) : (
+        <p>Select a county to begin</p>
+      )}
+    </Layout>
+  )
+}
 
 export default IndexPage
