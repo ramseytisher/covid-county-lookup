5088a2eb718d7011020ee81333b379901e695757
rt022748 <ramsey.tisher@cerner.com>
2020-09-17 13:48:24 -0400

    config changes

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,12 +8,12 @@ module.exports = {
   plugins: [
     `gatsby-plugin-react-helmet`,
     {
-      resolve: 'gatsby-plugin-web-font-loader',
+      resolve: "gatsby-plugin-web-font-loader",
       options: {
         google: {
-          families: ['Open Sans', 'Droid Serif']
-        }
-      }
+          families: ["Open Sans", "Droid Serif"],
+        },
+      },
     },
     {
       resolve: `gatsby-source-filesystem`,
@@ -38,7 +38,7 @@ module.exports = {
       options: {
         name: `COVID-19 County Tracker`,
         short_name: `covidCountyTr`,
-        start_url: `/`,
+        start_url: `/CovidCountyTracker/`,
         background_color: `#f0f2f5`,
         theme_color: `#f0f2f5`,
         display: `standalone`,
@@ -49,27 +49,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        // The property ID; the tracking code won't be generated without it
         trackingId: "UA-144985329-2",
-        // Defines where to place the tracking script - `true` in the head and `false` in the body
-        head: false,
-        // Setting this parameter is optional
-        anonymize: true,
-        // Setting this parameter is also optional
-        respectDNT: true,
-        // Avoids sending pageview hits from custom paths
-        exclude: ["/preview/**", "/do-not-track/me/too/"],
-        // Delays sending pageview hits on route update (in milliseconds)
-        pageTransitionDelay: 0,
-        // Enables Google Optimize using your container Id
-        optimizeId: "YOUR_GOOGLE_OPTIMIZE_TRACKING_ID",
-        // Enables Google Optimize Experiment ID
-        experimentId: "YOUR_GOOGLE_EXPERIMENT_ID",
-        // Set Variation ID. 0 for original 1,2,3....
-        variationId: "YOUR_GOOGLE_OPTIMIZE_VARIATION_ID",
-        // Defers execution of google analytics script after page load
-        defer: false,
-        // Any additional optional fields
       },
     },
   ],
